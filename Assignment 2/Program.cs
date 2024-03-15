558777f9a90f872d83b6b5bb52286329232fffea
shivamm-mehta <mehtas25@mymacewan.ca>
2024-03-14 22:36:01 -0600

    Assignment Submitted

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for (int num = 0; num < 10; num++)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"{num} is an even");
+                sumEven += num;
+            }
+            else
+            {
+                Console.WriteLine($"{num} is an odd");
+                sumOdd += num;
+            }
+        }
+
+        Console.WriteLine($"Total of even numbers: {sumEven}");
+        Console.WriteLine($"Total of odd numbers: {sumOdd}");
+    }
+}
